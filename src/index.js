5808364e0846531ef44c650e79caeb54e12d0454
gedadev <gjbg1234@gmail.com>
2023-01-20 21:02:00 -0600

    added a script to show and hide the dropdown menu, and added styles to the navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './style.css';
+
+let drop = document.querySelector('#drop');
+let subitems = document.querySelector('#submenu');
+
+class Navbar {
+    constructor(){
+        this.drop = drop;
+        this.subitems = subitems;
+    }
+    
+    submenu() {
+        drop.addEventListener('mouseover', () => {
+            if (subitems.classList.contains('visible')) {
+                subitems.classList.remove('visible');
+            } else {
+                subitems.classList.add('visible');
+            };
+        });    
+    }
+}
+
+let run = new Navbar(drop, subitems);
+run.submenu();
