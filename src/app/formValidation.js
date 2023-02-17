1f4702b295dcb4c472cf4a5b2f72d5dbeff9c24b
gedadev <gjbg1234@gmail.com>
2023-02-16 23:13:07 -0600

    added a method to validate the constraints of the inputs, and display the properly error in the dom

--- src/app/formValidation.js
+++ src/app/formValidation.js
@@ -10,14 +10,43 @@ export class Validations {
                 if (this.input[i].validity.valid) {
                     this.errorMsg[i].style.display = "none";
                 } else {
-                    Validations.inputValidation(this.input[i], this.errorMsg[i]);
+                    Validations.inputValidation(this.input[i], this.errorMsg[i], i);
                 }
             });
-            
+            this.input[i].addEventListener('focusout', () => {
+                this.errorMsg[i].style.display = "none";
+            });
+            this.input[i].addEventListener('focusin', () => {
+                Validations.inputValidation(this.input[i], this.errorMsg[i], i);
+            });
         }
     }
 
-    static inputValidation(currInput, currMsg) {
-
+    static inputValidation(currInput, currMsg, index) {
+        console.log(index);
+        if (currInput.validity.valueMissing) {
+            currMsg.textContent = "You must fill this field to continue";
+            currMsg.style.display = "block";
+        } else if (currInput.validity.tooShort) {
+            currMsg.textContent = `Password must be at least ${currInput.minLength} characters long`;
+            currMsg.style.display = "block";
+        }else if (currInput.validity.patternMismatch) {
+            switch (index) {
+                case 0:
+                    currMsg.textContent = "Enter a valid email address";
+                    currMsg.style.display = "block";
+                    break;
+                case 2:
+                    currMsg.textContent = "Enter a valid zip code";
+                    currMsg.style.display = "block";
+                    break;
+                case 3:
+                    currMsg.textContent = "Password must contain an upper case letter, a lower case and a number";
+                    currMsg.style.display = "block";
+                    break;    
+                default:
+                    break;
+            }
+        }
     }
 }
